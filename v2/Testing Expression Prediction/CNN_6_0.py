a2c51769052e0c731eac090615217ea636a8dd49
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-10-21 15:35:19 -0700

    Refactor, simplify CNN_6_0

--- v2/Testing Expression Prediction/CNN_6_0.py
+++ v2/Testing Expression Prediction/CNN_6_0.py
@@ -1,38 +1,26 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 from keras.models import Sequential, load_model # type: ignore
 from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense # type: ignore
 from keras.optimizers import Adam # type: ignore
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def load_and_preprocess_data(file_path):
+def load_features(file_path):
     df = pd.read_csv(file_path)
-    df['Normalized Observed log(TX/Txref)'] = MinMaxScaler().fit_transform(df[['Observed log(TX/Txref)']])
-    return df
-
-def combine_columns(df):
-    X = df[['Promoter Sequence']].astype(str).agg(''.join, axis=1)
-    y = df['Normalized Observed log(TX/Txref)']
+    y = MinMaxScaler().fit_transform(df[['Observed log(TX/Txref)']])
+    X = preprocess_sequences(df[['Promoter Sequence']].astype(str).agg(''.join, axis=1))
     return X, y
+
+def preprocess_sequences(X, max_length=150):
+    padded_sequences = [padded_one_hot_encode('0' * (max_length - len(seq)) + seq) for seq in X]
+    return np.array(padded_sequences)
 
 def padded_one_hot_encode(sequence):
     mapping = {'A': [1,0,0,0], 'C': [0,1,0,0], 'G': [0,0,1,0], 'T': [0,0,0,1], '0': [0,0,0,0]}
     encoding = [mapping[nucleotide.upper()] for nucleotide in sequence]
     return encoding
-
-def preprocess_sequences(X):
-    max_length = max(len(seq) for seq in X)
-    padded_sequences = [padded_one_hot_encode('0' * (max_length - len(seq)) + seq) for seq in X]
-    return np.array(padded_sequences), max_length
-
-def reshape_model_input(X):
-    return np.array([[x, x, x, x] for x in X.values]).reshape(-1, 1, 4)
-
-def concatenate_inputs(array1, array2):
-    return np.concatenate((array1, array2), axis=1)
 
 def build_cnn_model(input_shape):
     model = Sequential()
@@ -46,82 +34,35 @@ def build_cnn_model(input_shape):
     model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
     return model
 
-def train_model(model, X_train, y_train, X_test, y_test):
-    history = model.fit(X_train, y_train, epochs=150, batch_size=32, validation_data=(X_test, y_test))
+def train_model(model, X_train, y_train, X_test, y_test, epochs=150, batch_size=32):
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test))
     return history
 
-def evaluate_model(model, X_test, y_test):
-    loss = model.evaluate(X_test, y_test)
-    return loss
-
-def save_model(model, filename):
-    if not filename.endswith('.keras'):
-        filename += '.keras'
-    model.save('../Models/' + filename)
-
-def load_and_predict(filename, X):
-    if not filename.endswith('.keras'):
-        filename += '.keras'
-    model = load_model('../Models/' + filename)
+def load_and_predict(model_path, X):
+    model = load_model(model_path)
     predictions_array = model.predict(np.array(X))[:, 0]
     return pd.DataFrame(predictions_array, columns=['Value'])
 
-def ravel(array):
-    return np.ravel(array)
-
-def plot_kde(df, predicted):
-    sns.kdeplot(df['Normalized Observed log(TX/Txref)'], fill=True, color='blue', label='Observed log(TX/Txref)')
-    sns.kdeplot(predicted, fill=True, color='green', label='Our Prediction')
-    plt.title('Kernel Density Plot')
-    plt.xlabel('Value')
-    plt.ylabel('Density')
-    plt.legend()
-    plt.show()
-
-def plot_scatter(observed, predicted):
-    plt.figure(figsize=(10, 6))
-    plt.scatter(observed, predicted, color='blue', alpha=0.5, label='Data points')
-    min_val = min(min(observed), min(predicted))
-    max_val = max(max(observed), max(predicted))
-    plt.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--', label='y = x line')
-    plt.title('Observed log(TX/Txref) vs. Our Prediction')
-    plt.xlabel('Observed log(TX/Txref)')
-    plt.ylabel('Our Prediction')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-def plot_hexbin(observed, predicted):
-    plt.figure(figsize=(10, 6))
-    plt.hexbin(observed, predicted, gridsize=50, cmap='Blues', mincnt=1)
-    plt.colorbar(label='Counts')
-    plt.plot([0, 1], [0, 1], color='red', linestyle='--', label='y = x line')
-    plt.title('Observed log(TX/Txref) vs. Our Prediction')
-    plt.xlabel('Observed log(TX/Txref)')
-    plt.ylabel('Our Prediction')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-def main():
-    file_path = '../Data/41467_2022_32829_MOESM5_ESM.csv'
-    df = load_and_preprocess_data(file_path)
-    X, y = combine_columns(df)
-    X, max_length = preprocess_sequences(X)
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = build_cnn_model(X.shape[1:])
-    
-    history = train_model(model, X_train, y_train, X_test, y_test)
-    loss = evaluate_model(model, X_test, y_test)
-    save_model(model, 'CNN_1_3_1.keras')
-    
-    predicted = load_and_predict('CNN_1_3_1.keras', X)
-    observed = df['Normalized Observed log(TX/Txref)'].values
-    
-    plot_kde(df, predicted)
-    plot_scatter(observed, np.ravel(predicted))
-    plot_hexbin(observed, np.ravel(predicted))
+def calc_metrics(y_test, y_pred):
+    mse = np.mean((y_test - y_pred) ** 2)
+    rmse = np.sqrt(mse)
+    mae = np.mean(np.abs(y_test - y_pred))
+    r2 = 1 - mse / np.var(y_test)
+    return mse, rmse, mae, r2
 
 if __name__ == "__main__":
-    main()
+    model_path = '../Models/CNN_6_0.keras'
+    X_train, y_train = load_features('../Data/Train Test/train_data.csv')
+    X_test, y_test = load_features('../Data/Train Test/test_data.csv')
+    model = build_cnn_model(X_train.shape[1:])
+    history = train_model(model, X_train, y_train, X_test, y_test)
+
+    model.save(model_path)
+    y_pred = load_and_predict(model_path, X_test)
+
+    mse, rmse, mae, r2 = calc_metrics(y_test, y_pred)
+
+    print('MSE: ', mse)
+    print('RMSE: ', rmse)
+    print('MAE: ', mae)
+    print('R2: ', r2)

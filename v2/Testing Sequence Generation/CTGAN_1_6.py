b013ec1416249a3ef444b6da692b3574ea2ce809
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-10-24 21:41:17 -0700

    Update CTGAN_1_6.py

--- v2/Testing Sequence Generation/CTGAN_1_6.py
+++ v2/Testing Sequence Generation/CTGAN_1_6.py
@@ -2,10 +2,8 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
-from sklearn.preprocessing import MinMaxScaler
 import random
 import keras
-from sklearn.model_selection import train_test_split
 
 def load_data(file_path):
     return pd.read_csv(file_path)

394cd51141a9ef2b9c0704450323618942e4170e
Rhys Sorenson-Graff <sorensor@whitman.edu>
2025-02-27 20:39:46 -0800

    Testing RL and multiple-environment RL

--- v2/Testing GA/GA_deap/GA/ReinforcementLearning.py
+++ v2/Testing GA/GA_deap/GA/ReinforcementLearning.py
@@ -4,17 +4,18 @@ import os
 import gym
 from gym import spaces
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
 from .CNN import CNN
 
 class PromoterOptimizationEnv(gym.Env):
-    """
+    '''
     Custom OpenAI Gym environment for optimizing promoter sequences using Reinforcement Learning.
-    """
+    '''
 
     def __init__(self, cnn_model_path, masked_sequence, target_expression, max_steps=20, seed=None):
         super(PromoterOptimizationEnv, self).__init__()
-        
+
         if seed is not None:
             self._set_seed(seed)
 
@@ -45,9 +46,7 @@ class PromoterOptimizationEnv(gym.Env):
         return [i for i, element in enumerate(masked_sequence) if np.allclose(element, 0.25, atol=1e-9)]
 
     def _decode_action(self, action):
-        """
-        Convert an action index into a (position, nucleotide) change.
-        """
+        '''Convert an action index into a (position, nucleotide) change.'''
         position_index = action // 4
         nucleotide_index = action % 4
 
@@ -57,9 +56,7 @@ class PromoterOptimizationEnv(gym.Env):
         return self.mask_indices[position_index], nucleotide_index
 
     def step(self, action):
-        """
-        Apply the action (change one nucleotide) and compute the new reward.
-        """
+        '''Apply the action (change one nucleotide) and compute the new reward.'''
         self.current_step += 1
 
         # Decode action
@@ -83,10 +80,7 @@ class PromoterOptimizationEnv(gym.Env):
         return self.state, reward, done, {}
 
     def reset(self):
-        """
-        Reset the environment to the initial masked sequence.
-        Ensure that masked positions are initialized randomly.
-        """
+        '''Reset the environment with a randomly initialized masked sequence.'''
         self.current_step = 0
         self.state = self.masked_sequence.copy()
         
@@ -99,18 +93,21 @@ class PromoterOptimizationEnv(gym.Env):
         
         return self.state
 
+def make_env(cnn_model_path, masked_sequence, target_expression):
+    ''' Create multiple environments for training.'''
+    return lambda: PromoterOptimizationEnv(cnn_model_path, masked_sequence, target_expression)
 
-# Train RL Agent using PPO
-def train_rl_agent(cnn_model_path, masked_sequence, target_expression, total_timesteps=50000):
-    env = DummyVecEnv([lambda: PromoterOptimizationEnv(cnn_model_path, masked_sequence, target_expression)])
+def train_rl_agent(cnn_model_path, masked_sequences, target_expression, total_timesteps=50000, num_envs=4):
+    ''' Trains an RL agent using PPO on multiple masked sequences. '''
+    envs = SubprocVecEnv([make_env(cnn_model_path, seq, target_expression) for seq in masked_sequences[:min(num_envs, len(masked_sequences))]])
 
-    model = PPO("MlpPolicy", env, verbose=1)
+    model = PPO('MlpPolicy', envs, verbose=1)
     model.learn(total_timesteps=total_timesteps)
 
     return model
 
-# Optimize sequence using the trained RL agent
 def optimize_sequence_with_rl(model, cnn_model_path, masked_sequence, target_expression):
+    ''' Optimize a masked sequence using a trained RL agent.'''
     env = PromoterOptimizationEnv(cnn_model_path, masked_sequence, target_expression)
     obs = env.reset()
     
@@ -128,6 +125,6 @@ def optimize_sequence_with_rl(model, cnn_model_path, masked_sequence, target_exp
 
     return optimized_sequence, prediction, error, env.state
 
-
+# Load a pre-trained RL agent
 def load_rl_agent(model_path):
     return PPO.load(model_path)
